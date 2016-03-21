1894c0105fb74bab7647fba695e76937c6ab3ceb
ibmwtx <sdandamu@us.ibm.com>
2016-03-21 12:41:38 -0400

    Fixed Issue with File Path

--- v9.0.0.0/run.sh
+++ v9.0.0.0/run.sh
@@ -2,7 +2,15 @@
 
 echo $(hostname -I)  $(HOSTNAME) >> /etc/hosts
 
-source /home/ec2-user/bvt9000_64/wtx_8412/setup
+source /home/ec2-user/bvt9000_64/wtx_9000/setup
 
-/home/ec2-user/bvt8412_64/wtx_9000/bin/launcher.sh -start
+/home/ec2-user/bvt9000_64/wtx_9000/bin/launcheradmin.sh -adduser "user:admin;login:admin;pwd:admin;access:gss,gpr,gm"
+
+/home/ec2-user/bvt9000_64/wtx_9000/bin/launcheradmin.sh -firewall enable
+
+/home/ec2-user/bvt9000_64/wtx_9000/bin/launcheradmin.sh -addprop java.rmi.useLocalHostname:false
+
+/home/ec2-user/bvt9000_64/wtx_9000/bin/launcheradmin.sh -addprop java.rmi.server.hostname:10.0.0.11
+
+/home/ec2-user/bvt9000_64/wtx_9000/bin/launcher.sh -start
 
